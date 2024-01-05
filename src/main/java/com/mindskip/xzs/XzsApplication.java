29a8a183f123acbde6c27fd57966b3e1aea9f790
湖南华测教育 <133788554+hctestedu@users.noreply.github.com>
2024-01-05 14:57:35 +0800

    Add files via upload

---
+++ src/main/java/com/mindskip/xzs/XzsApplication.java
@@ -0,0 +1,28 @@
+package com.mindskip.xzs;
+
+import com.mindskip.xzs.configuration.property.SystemConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @version 3.5.0
+ * @description: The type Xzs application.
+ * Copyright (C), 2020-2024, 武汉思维跳跃科技有限公司
+ * @date 2021/12/25 9:45
+ */
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableConfigurationProperties(value = { SystemConfig.class})
+public class XzsApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(XzsApplication.class, args);
+    }
+}
